86a89b65ace4fe33467b84a06d7d7c7f885db5cb
Plak1n <ms.joker590@gmail.com>
2026-03-24 15:01:55 +0300

    rework

---
+++ freebie_coffie/my-verstka/html/js/app.js
@@ -0,0 +1,9 @@
+document.addEventListener('mousemove', event => {
+	Object.assign(document.documentElement, {
+		style: `
+		--move-x: ${(event.clientX - window.innerWidth / 2) * -.005}deg;
+		--move-y: ${(event.clientY - window.innerHeight / 2) * .01}deg;
+		`
+	})
+})
+v
